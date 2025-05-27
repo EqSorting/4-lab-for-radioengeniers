8ac1fb917798fd23dff57ce6cdf53fe10a424e9e
EqSorting <alexvlasov23@mail.ru>
2025-05-27 16:06:46 +0700

    Были исправлены о

--- Varianti/variant6.cpp
+++ Varianti/variant6.cpp
@@ -4,77 +4,91 @@
 #include <ctime>
 using namespace std;
 
-int min_element(int *array, int size){
-    int min = array[0];
-    for (int i = 1; i < size; i++){
-        if (array[i] < min) {
-            min = array[i];
-        }
-    }
-    cout << "Min element is: " << min << endl;
+void min_element(long double *array, int size){
+    int min = array[0];
+    for (int i = 1; i < size; i++){
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    cout << "Min element is: " << min << endl;
 }
 
-int sum_elements(int *array, int size){
-    int sum = 0;
-    int min_index;
-    int max_index;
-    for (int i = 0; i < size; i++){
-        if (array[i] > 0){
-            min_index = i;
-        }
-    }
+void sum_elements(long double *array, int size){
+    float implication = 0;
+    int min_index = -1;
+    int max_index = -1;
+    int count = 0;
+    for (int i = 0; i < size; i++){
+        if (array[i] > 0){
+            count++;
+            min_index = i;
+            break;
+        }
+    }
 
-    for (int i = size-1; i > 0; i++){
-        if (array[i] > 0){
-            max_index = i;
-        }
-    }
+    for (int i = size; i > 0; i--){
+        if (array[i] > 0){
+            count++;
+            max_index = i;
+            break;
+        }
+    }
 
-    int start = min_index;
-    int end = max_index;
 
-    for (int i = start-1 ; i < end; i++){
-        sum += array[i];
-    }
+    if (min_index == -1 && max_index == -1){
+        cout << "Not found positivity element or found one of them is" << endl;
+        implication = 0;
+    }
+    else if (min_index != -1 && max_index == -1) {
+        cout << "Found only one positivity element" << endl;
+        implication = array[min_index];
+    }
+    else {
+        for (int i = min_index+1; i < max_index; i++){
+            implication += array[i];
+        }
+    }
 
-    cout << "Sum of elements is: " << sum << endl;
+    cout << "Sum of element " << implication << endl;
 }
 
-int sorting(int *array, int size){
-    int array2[size];
-    int posIndex = 0;
-    int negIndex = size - 1;
-    
-    for (int i = 0; i < size; i++){
-        if (array[i] == 0){
-            array2[posIndex++] = array[i];
-        } 
-        else{
-            array2[negIndex--] = array[i];
-        }
-    }
-    
-    for (int i = 0; i < size; i++){
-        cout << array2[i] << " ";
-    }
-    }
+void sorting(long double *array, int size){
+    long double *array2 = (long double *)malloc(size * sizeof(long double));
+    int posIndex = 0;
+    int negIndex = size - 1;
+
+    for (int i = 0; i < size; i++){
+        if (array[i] == 0){
+            array2[posIndex++] = array[i];
+        }
+        else{
+            array2[negIndex--] = array[i];
+        }
+    }
+ 
+    for (int i = 0; i < size; i++){
+        cout << array2[i] << " ";
+    }
+    free(array2);
+}
 
 int main(){
-    int size;
-    srand(time(0));
-    cout << "Enter the size of the array: ";
-    cin >> size;
-    int array[size];
-    for (int i = 0; i < size; i++){
-        array[i] = rand() % 20-10; 
-    }
+    int size;
+    srand(time(0));
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    long double array[size];
+    for (int i = 0; i < size; i++){
+        array[i] = rand() % 20-10; 
+    }
 
-    for (int i = 0; i < size; i++){
-        std::cout << array[i] << " ";
-    }
-    cout << endl;
-    min_element(array, size);
-    sum_elements(array, size);
-    sorting(array, size);
-    return 0;
+    for (int i = 0; i < size; i++){
+        std::cout << array[i] << " ";
+    }
+    cout << endl;
+    min_element(array, size);
+    sum_elements(array, size);
+    sorting(array, size);
+    return 0;
 }
